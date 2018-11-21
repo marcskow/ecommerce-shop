d0efc8ed8ee027cadb359f05db24c6d7eddb77e5
Marcin Skowron <mskowron17@gmail.com>
2018-11-21 19:43:08 +0100

    Remove image style from product component to reuse bootstrap defaults.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -4,15 +4,7 @@ import { Product } from './product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styles: [`
-  img {
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    padding: 5px;
-    width: 150px;
-    height: 150px;
-  }
-  `]
+  styles: []
 })
 export class ProductComponent implements OnInit {
 
